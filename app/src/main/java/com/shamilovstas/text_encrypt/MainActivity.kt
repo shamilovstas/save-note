a437bc1c527aee2a7541e0462284a11c477df5be
Stanislav Shamilov <shamilovstas@protonmail.com>
2023-12-23 20:08:08 +0200

    item #2: added intent filter (as it is not possible to add a filter for a file extension, this filter effectively opens any file that is application/octet-stream) to open exported notes and a handler for wrong file type

--- app/src/main/java/com/shamilovstas/text_encrypt/MainActivity.kt
+++ app/src/main/java/com/shamilovstas/text_encrypt/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.shamilovstas.text_encrypt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavHost
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.shamilovstas.text_encrypt.base.AppBarConfigurationProvider
+import com.shamilovstas.text_encrypt.notes.compose.ComposeNoteFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,6 +31,18 @@ class MainActivity : AppCompatActivity(), AppBarConfigurationProvider {
         )
         appBarConfiguration = AppBarConfiguration(topLevel, drawerLayout)
         findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
+
+        checkForImportedFile(navController, intent)
+    }
+
+    private fun checkForImportedFile(navController: NavController, intent: Intent?) {
+        if (intent == null) return
+
+        if (intent.action == Intent.ACTION_VIEW) {
+            val uri = intent.data ?: return
+
+            navController.navigate(R.id.action_import_file, ComposeNoteFragment.fileImportArgs(uri))
+        }
     }
 
     override fun provideAppBarConfiguration(): AppBarConfiguration {

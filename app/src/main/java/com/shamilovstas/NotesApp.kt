3657057de3cfedf6a76c8107ae61c31ed1d8e138
Stanislav Shamilov <shamilovstas@protonmail.com>
2023-12-24 01:22:13 +0200

    item #6: decrypted attachment now can be saved to a directory specified by the user

--- app/src/main/java/com/shamilovstas/NotesApp.kt
+++ app/src/main/java/com/shamilovstas/NotesApp.kt
@@ -1,8 +1,15 @@
 package com.shamilovstas
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NotesApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder().build())
+    }
 }
